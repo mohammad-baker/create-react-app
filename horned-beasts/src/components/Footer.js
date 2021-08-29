9021e91a58ffd863380fd8e4ed6b0174ec631d82
mohammad <altaeabbaker@gmail.com>
2021-08-29 16:41:35 +0300

    adfgbh

---
+++ horned-beasts/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+class Footer extends React.Component{
+
+    render() {
+        return (
+            <footer>
+            <center>
+            <h5>
+                mohammad baker
+            </h5>
+            </center>
+            </footer>   
+        )
+    }0
+}
+export default Footer;
---
+++ horned-beasts/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+class Header extends React.Component{
+
+    render() {
+        return (
+            <header>
+            <center>
+            <h1>
+                HornedBeasts
+            </h1>
+            </center>
+            </header>   
+        )
+    }
+}
+export default Header;
