f2e9bfdf568b89d55f125202482f01d8851a8d29
mohammad <altaeabbaker@gmail.com>
2021-08-30 17:25:17 +0300

    As

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
